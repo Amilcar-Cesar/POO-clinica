e1e5322e362065782b0457cc0c984e7cf403d21a
Amilcar Cesar <amilcarcesar0@yahoo.com.br>
2025-06-17 16:56:47 -0300

    Menu aprimorado

--- main.py
+++ main.py
@@ -8,47 +8,95 @@ def menu_principal():
     minha_clinica = Clinica()
     while True:
         print("\n--- Sistema de Gerenciamento da Clínica ---")
-        print("--- Pacientes ---")
-        print("1. Adicionar Paciente | 2. Listar Pacientes")
-        print("--- Especialidades ---")
-        print("3. Adicionar Especialidade | 4. Listar Especialidades")
-        print("--- Atendimentos ---")
-        print("5. Agendar Atendimento")
-        print("6. Listar Atendimentos")
-        print("7. Atualizar Status de Atendimento")
-        print("\n8. Sair")
+        print("1 - Pacientes")
+        print("2 - Especialidades")
+        print("3 - Atendimentos")
+        print("4. Sair")
+        print("===============================")
         
         opcao = input("Escolha uma opção: ")
-
+        
+        
         if opcao == '1':
-            nome = input("Nome do paciente: ")
-            data = input("Data de nascimento (DD/MM/AAAA): ")
-            tel = input("Telefone: ")
-            minha_clinica.adicionar_paciente(nome, data, tel)
+            print("\n--- Gerenciar Pacientes ---")
+            print("1 - Adicionar paciente")
+            print("2 - Listar pacientes")
+            print("3 - Atualizar paciente")
+            print("4 - Remover paciente")
+            
+            op_paciente = input("Digite qual ação a ser feita: ")
+
+            if op_paciente == '1':
+                print("\n--- Novo paciente ---")
+                nome = input("Nome do paciente: ")
+                data = input("Data de nascimento (DD/MM/AAAA): ")
+                tel = input("Telefone: ")
+                minha_clinica.adicionar_paciente(nome, data, tel)
+            elif op_paciente == '2':
+                minha_clinica.listar_pacientes()
+            elif op_paciente == '3':
+                # CORRIGIDO: Faltava pedir o ID
+                id_paciente = int(input("Digite o ID do paciente a ser atualizado: "))
+                minha_clinica.atualizar_paciente(id_paciente)
+            elif op_paciente == '4': # CORRIGIDO: Verificando op_paciente em vez de opcao
+                # CORRIGIDO: Faltava pedir o ID
+                id_paciente = int(input("Digite o ID do paciente a ser removido: "))
+                minha_clinica.remover_paciente(id_paciente)
+            
+        # CORRIGIDO: Usando ELIF
         elif opcao == '2':
-            minha_clinica.listar_pacientes()
+            print("\n--- Gerenciar Especialidades ---")
+            print("1 - Adicionar especialidade")
+            print("2 - Listar especialidades")
+            print("3 - Atualizar especialidade")
+            print("4 - Remover especialidade")
+            
+            op_espec = input("Digite o número da ação que deseja: ")
+
+            if op_espec == '1':
+                especialidade = input("Nome da nova especialidade: ")
+                minha_clinica.adicionar_especialidade(especialidade)
+            elif op_espec == '2':
+                
+                minha_clinica.listar_especialidade()
+            elif op_espec == '3':
+                 
+                id_espec = int(input("Digite o ID da especialidade a ser atualizada: "))
+                minha_clinica.atualizar_especialidade(id_espec)
+            elif op_espec == '4':
+            
+                id_remover = int(input("Informe o ID da especialidade a ser removida: "))
+                minha_clinica.remover_especialidade(id_remover)
+
+        
         elif opcao == '3':
-            nome_espec = input("Nome da especialidade: ")
-            minha_clinica.adicionar_especialidade(nome_espec)
+            print("\n--- Gerenciar Atendimentos ---")
+            print("1 - Agendar atendimento")
+            print("2 - Listar atendimentos")
+            print("3 - Atualizar status")
+            
+            op_atend = input("Qual ação deseja realizar? ")
+
+            if op_atend == '1':
+                print("\n--- Agendar Novo Atendimento ---")
+                minha_clinica.listar_pacientes()
+                minha_clinica.listar_especialidade() 
+                id_paciente = int(input("Digite o ID do Paciente: ")) 
+                id_espec = int(input("Digite o ID da Especialidade: "))
+                minha_clinica.agendar_atendimento(id_paciente, id_espec)
+            elif op_atend == '2': 
+                minha_clinica.listar_atendimentos()
+            elif op_atend == '3':
+                id_atend = int(input("Digite o ID do Atendimento a ser atualizado: "))
+                novo_status = input("Digite o novo status (Ex: Realizado, Cancelado): ")
+                minha_clinica.atualizar_status_atendimento(id_atend, novo_status)
+        
+        # CORRIGIDO: Usando ELIF
         elif opcao == '4':
-            minha_clinica.listar_especialidade()
-        elif opcao == '5':
-            print("\n--- Agendar Novo Atendimento ---")
-            # Para facilitar, listamos as opções disponíveis para o usuário
-            minha_clinica.listar_pacientes()
-            minha_clinica.listar_especialidade()
-            id_paciente = int(input("Digite o ID do Paciente: "))
-            id_espec = int(input("Digite o ID da Especialidade: "))
-            minha_clinica.agendar_atendimento(id_paciente, id_espec)
-        elif opcao == '6':
-            minha_clinica.listar_atendimentos()
-        elif opcao == '7':
-            id_atend = int(input("Digite o ID do Atendimento a ser atualizado: "))
-            novo_status = input("Digite o novo status (Ex: Realizado, Cancelado): ")
-            minha_clinica.atualizar_status_atendimento(id_atend, novo_status)
-        elif opcao == '8':
             print("Saindo do sistema. Até logo!")
             break
+        
+        # Agora este ELSE só será executado se nenhuma das opções (1, 2, 3, 4) for escolhida.
         else:
             print("Opção inválida. Tente novamente.")
 
